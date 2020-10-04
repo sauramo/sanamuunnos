6e7e248845c8cfee75ef9d980f6f4ca65073aded
Miikka Sauramo <miikka.sauramo@phz.fi>
2020-10-04 23:28:38 +0300

    test

--- miikka/src/test/java/sauramo/AppTest.java
+++ miikka/src/test/java/sauramo/AppTest.java
@@ -6,18 +6,18 @@ import org.junit.Test;
 
 public class AppTest {
 
-	@Test
-	public void testGivenExamples() {
-		App app = new App();
-		assertEquals("Trunald Domp", app.muunna("Donald Trump"));
-		assertEquals("mäörkage vuoilnö", app.muunna("vuoirkage mäölnö"));
-		assertEquals("fath, deamine, pend astilence", app.muunna("death, famine, and pestilence"));
-		assertEquals("ba foor baz", app.muunna("foo bar baz"));
-		assertEquals("bama foorbu", app.muunna("fooma barbu"));
-		assertEquals("bomama  amomo foo", app.muunna("amama  bomomo foo"));
-		assertEquals("Supercalifragilisticexpialidocious!", app.muunna("Supercalifragilisticexpialidocious!"));
+  @Test
+  public void testGivenExamples() {
+    App app = new App();
+    assertEquals("Trunald Domp", app.muunna("Donald Trump"));
+    assertEquals("mäörkage vuoilnö", app.muunna("vuoirkage mäölnö"));
+    assertEquals("fath, deamine, pend astilence", app.muunna("death, famine, and pestilence"));
+    assertEquals("ba foor baz", app.muunna("foo bar baz"));
+    assertEquals("bama foorbu", app.muunna("fooma barbu"));
+    assertEquals("bomama  amomo foo", app.muunna("amama  bomomo foo"));
+    assertEquals("Supercalifragilisticexpialidocious!", app.muunna("Supercalifragilisticexpialidocious!"));
     assertEquals("ra'd Ither he diere.", app.muunna("I'd rather die here."));
     assertEquals("šysar qraevgfvå eo", app.muunna("qraesar šyvgfvå eo"));
-    assertEquals("ošyĸnfn eäwtxqyuthäuny omäbw eodkkafudhcfiuc", app.muunna("eäšyĸnfn owtxqyuthäuny eomäbw odkkafudhcfiuc"));    
-	}
+    assertEquals("ošyĸnfn eäwtxqyuthäuny omäbw eodkkafudhcfiuc", app.muunna("eäšyĸnfn owtxqyuthäuny eomäbw odkkafudhcfiuc"));
+  }
 }
