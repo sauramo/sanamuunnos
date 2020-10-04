abb92cf812b75dca441a15e3a08a8c3393e7df63
Miikka Sauramo <miikka.sauramo@phz.fi>
2020-10-04 23:26:48 +0300

    Better naming, cleanup.

--- miikka/src/main/java/sauramo/App.java
+++ miikka/src/main/java/sauramo/App.java
@@ -11,30 +11,30 @@ import java.util.stream.Collectors;
 
 public class App {
 
-  public String muunna(final String stringToTranspose) {
+  public String muunna(String stringToTranspose) {
     
-    String[] wordsIncludingTrailingWhitespace = stringToTranspose.split("\\s(?=\\S)");
+    String[] words = stringToTranspose.split("\\s(?=\\S)");
 
-    if (wordsIncludingTrailingWhitespace.length < 2) { // trivial case of one word
+    if (words.length < 2) { // trivial case of one word
       return stringToTranspose;
     }
 
     String oddword = "", finalResult = "", rearrangedWordPairs = "";
 
-    if (wordsIncludingTrailingWhitespace.length % 2 != 0) { // check if words have odd or even count and store last value for end result
-      oddword = wordsIncludingTrailingWhitespace[wordsIncludingTrailingWhitespace.length - 1]; // after removing it from processing
-      wordsIncludingTrailingWhitespace = Arrays.copyOf(wordsIncludingTrailingWhitespace, wordsIncludingTrailingWhitespace.length - 1);
+    if (words.length % 2 != 0) {          // Store last word for end result
+      oddword = words[words.length - 1];  // if count of words is not even
+      words = Arrays.copyOf(words, words.length - 1);
     }
 
-    final List<String> result = new ArrayList<String>();
+    List<String> result = new ArrayList<String>();
     
-    for (final String ss : wordsIncludingTrailingWhitespace) {
-      final Pattern pattern = Pattern.compile("[aeiouyäåö]", Pattern.CASE_INSENSITIVE);
-      final Matcher matchFirstVowel = pattern.matcher(ss);
+    for (String wordToProcess : words) {
+      Pattern pattern = Pattern.compile("[aeiouyäåö]", Pattern.CASE_INSENSITIVE);
+      Matcher matchFirstVowel = pattern.matcher(wordToProcess);
 
       while (matchFirstVowel.find()) {
-        final String lettersUntilFirstVowel = ss.substring(0, matchFirstVowel.end());
-        final String lettersAfterFirstVowel = ss.substring(matchFirstVowel.end(), ss.length());
+        String lettersUntilFirstVowel = wordToProcess.substring(0, matchFirstVowel.end());
+        String lettersAfterFirstVowel = wordToProcess.substring(matchFirstVowel.end(), wordToProcess.length());
         Matcher matchConsecutiveVowels = pattern.matcher(lettersAfterFirstVowel);
         if (lettersAfterFirstVowel.length() > 0) {
           matchConsecutiveVowels = pattern.matcher(lettersAfterFirstVowel.substring(0, 1));
@@ -51,8 +51,9 @@ public class App {
     Collection<List<String>> partitionedList = partitionToSize(result, 4);
 
     int count = 0;
-    for (final List<String> groupOfFours : partitionedList) {
-      String rearrangedParts = groupOfFours.get(2) + groupOfFours.get(1) + " " +groupOfFours.get(0) + groupOfFours.get(3) + " ";
+    for (List<String> groupOfFours : partitionedList) {
+      String rearrangedParts = groupOfFours.get(2) + groupOfFours.get(1) + " " +
+                               groupOfFours.get(0) + groupOfFours.get(3) + " ";
       rearrangedWordPairs += rearrangedParts;
       count++;
       if (count == partitionedList.size()) { // add to constructed string at last iteration only
