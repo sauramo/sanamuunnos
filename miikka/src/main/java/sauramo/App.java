9ce51d4b91f36bae23045b4ec01dd6ac82050812
Miikka Sauramo <miikka.sauramo@phz.fi>
2020-10-04 23:06:33 +0300

    More informative naming of variables. Remove unnecessary string array.

--- miikka/src/main/java/sauramo/App.java
+++ miikka/src/main/java/sauramo/App.java
@@ -19,7 +19,7 @@ public class App {
       return stringToTranspose;
     }
 
-    String oddword = "";
+    String oddword = "", finalResult = "", rearrangedWordPairs = "";
 
     if (wordsIncludingTrailingWhitespace.length % 2 != 0) { // check if words have odd or even count and store last value for end result
       oddword = wordsIncludingTrailingWhitespace[wordsIncludingTrailingWhitespace.length - 1]; // after removing it from processing
@@ -30,21 +30,19 @@ public class App {
     
     for (final String ss : wordsIncludingTrailingWhitespace) {
       final Pattern pattern = Pattern.compile("[aeiouyäåö]", Pattern.CASE_INSENSITIVE);
-      final Matcher matcher = pattern.matcher(ss);
+      final Matcher matchFirstVowel = pattern.matcher(ss);
 
-      while (matcher.find()) {
-        final String first = ss.substring(0, matcher.end());
-        final String last = ss.substring(matcher.end(), ss.length());
-        final Matcher matcher2;
-        if (last.length() > 0) {
-          matcher2 = pattern.matcher(last.substring(0, 1));
-        } else {
-          matcher2 = pattern.matcher(last);
+      while (matchFirstVowel.find()) {
+        final String lettersUntilFirstVowel = ss.substring(0, matchFirstVowel.end());
+        final String lettersAfterFirstVowel = ss.substring(matchFirstVowel.end(), ss.length());
+        Matcher matchConsecutiveVowels = pattern.matcher(lettersAfterFirstVowel);
+        if (lettersAfterFirstVowel.length() > 0) {
+          matchConsecutiveVowels = pattern.matcher(lettersAfterFirstVowel.substring(0, 1));
         }
 
-        if (!matcher2.find()) {
-          result.add(first);
-          result.add(last);
+        if (!matchConsecutiveVowels.find()) {
+          result.add(lettersUntilFirstVowel);
+          result.add(lettersAfterFirstVowel);
           break;
         }
       }
@@ -52,12 +50,9 @@ public class App {
 
     Collection<List<String>> partitionedList = partitionToSize(result, 4);
 
-    String finalResult = "";
-    String rearrangedWordPairs = "";
     int count = 0;
     for (final List<String> groupOfFours : partitionedList) {
-      final String[] stringArray = groupOfFours.stream().toArray(String[]::new);
-      String rearrangedParts = stringArray[2] + stringArray[1] + " " + stringArray[0] + stringArray[3] + " ";
+      String rearrangedParts = groupOfFours.get(2) + groupOfFours.get(1) + " " +groupOfFours.get(0) + groupOfFours.get(3) + " ";
       rearrangedWordPairs += rearrangedParts;
       count++;
       if (count == partitionedList.size()) { // add to constructed string at last iteration only
